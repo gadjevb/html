258d767dd84b19c8485d8f2d44bc34eff2cf0983
Borislav Gadjev <gadjevb@gmail.com>
2016-09-13 14:00:18 +0300

    Fixes

--- validator.js
+++ validator.js
@@ -1,22 +1,8 @@
 function validator() {
 	var fraction = document.getElementById("frac").value;
-	var array = fraction.split(/[.,]+/);
-	if(array.length == 2){
-		var left = array[0];
-		var right = array[1];
-		if(left.length > 0 && left.length < 11 && right.length > 0 && right.length < 6 && left.match(/^\d+$/) && right.match(/^\d+$/)) {
-			alert("Valid!");
-		}else{
-			alert("Not valid!");
-		}
-	}else if(array.length == 1){
-		var left = array[0];
-		if(left.length > 0 && left.length < 11 && left.match(/^\d+$/))	{
-			alert("Valid!");
-		}else{
-			alert("Not valid!");
-		}
+	if(fraction.match(/\b[0-9]{1,10}[.,]{1}[0-9]{1,5}\b/g) || fraction.match(/^[0-9]{1,10}$/g)) {
+		alert("Valid");
 	}else{
-		alert("Not valid!");
+		alert("Not valid");
 	}
 }
